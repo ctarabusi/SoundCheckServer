a9f6089cece159e2ba5014ecd815b453f3bc88bd
Christian Tarabusi <cta@sinnerschrader-mobile.com>
2015-11-30 16:27:48 +0100

    Updated recording servlet

--- src/main/java/s2m/fourier/RecordingServlet.java
+++ src/main/java/s2m/fourier/RecordingServlet.java
@@ -1,87 +1,43 @@
 package s2m.fourier;
 
-import org.apache.commons.math.complex.Complex;
-import org.apache.commons.math.transform.FastFourierTransformer;
-
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.ShortBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.InputStream;
 
 public class RecordingServlet extends HttpServlet
 {
+    public static String RECORDING_AUDIO_PATH = "/WEB-INF/Recording.wav";
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        BufferedInputStream in = null;
+        ServletContext context = getServletContext();
+        String fullPath = context.getRealPath(RECORDING_AUDIO_PATH);
 
-        byte[] buffer = new byte[10240];
 
-        List<Double> inputFFTList = new ArrayList<Double>();
+        InputStream is = req.getInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(fullPath));
+
         try
         {
-            in = new BufferedInputStream(req.getInputStream());
-
-            for (int length = 0; (length = in.read(buffer, 0, buffer.length)) > 0; )
+            byte[] buf = new byte[4096];
+            for (int nChunk = is.read(buf); nChunk != -1; nChunk = is.read(buf))
             {
-                ShortBuffer shortBuffer = ByteBuffer.wrap(buffer, 0, length).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-
-                short[] samplesArray = new short[shortBuffer.limit()];
-                shortBuffer.get(samplesArray);
-
-                int index = 0;
-                for (short sample : samplesArray)
-                {
-                    inputFFTList.add((double) (hammingWindow(samplesArray.length, index) * sample));
-                    index++;
-                }
+                fileOutputStream.write(buf, 0, nChunk);
             }
         }
         finally
         {
-            if (in != null)
-            {
-                in.close();
-            }
+            is.close();
+            fileOutputStream.flush();
+            fileOutputStream.close();
         }
-
-
-        double[] inputArray = ServletUtils.doubleArrayToPrimitve(inputFFTList);
-        double[] inputWithoutMeanFFT = ServletUtils.removeAverage(inputArray);
-        double[] inputFFT = ServletUtils.addZeroPaddingToPowerTwo(inputWithoutMeanFFT);
-        Complex[] outputFFT = new FastFourierTransformer().transform(inputFFT);
-
-        ServletOutputStream outputStream = resp.getOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-
-        Complex[] complexArray = Arrays.copyOfRange(outputFFT, 0, outputFFT.length / 2);
-        double[] magnitudeArray = ServletUtils.getMagnitudeComponents(complexArray);
-        objectOutputStream.writeObject(magnitudeArray);
-
-        outputStream.flush();
-        outputStream.close();
-    }
-
-
-
-    public static float hammingWindow(int length, int index)
-    {
-        if (index > length)
-        {
-            return 0;
-        }
-        return 0.54f - 0.46f * (float) Math.cos(Math.PI * 2 * index / (length - 1));
     }
 
 }
